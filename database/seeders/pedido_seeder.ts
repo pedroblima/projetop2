dda870419c69f419e05e896cb7cad7238fb6bfe2
pedroblima <pedrob@iesb.edu.br>
2024-06-04 18:14:47 -0300

    .

--- database/seeders/pedido_seeder.ts
+++ database/seeders/pedido_seeder.ts
@@ -7,17 +7,17 @@ export default class PedidoSeeder extends BaseSeeder {
     await Pedido.createMany([
       {
         clientes_id: 1, 
-        dt_pedido: new Date(), 
+        dt_pedido: new Date(2024, 4, 6), 
         status: 'Processando'
       },
       {
         clientes_id: 2, 
-        dt_pedido: new Date(), 
+        dt_pedido: new Date( 2024, 4, 7), 
         status: 'Enviado'
       },
       {
         clientes_id: 3, 
-        dt_pedido: new Date(), 
+        dt_pedido: new Date( 2048, 4, 8),  
         status: 'Entregue'
       }
     ])
